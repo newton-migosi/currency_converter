670c9d9ee3ad8f0bfd07a617d86d776c56ee9597
toni_sheen <newtonmigosi@gmail.com>
2018-06-28 22:50:03 +0300

    made select options dynamic, refactored code

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
-const NAMES  = {
+const NAMES = {
     'USD': "United State Dollars",
     'KES': "Kenyan Shillings",
     'USH': "Ugandan Shillings",
     'RSA': "South African Rand",
-    'TSH': "Tanzanian Shillings"
-}
+    'TSH': "Tanzanian Shillings",
+    'EUR': "Euro"
+};
 
 const RATES = {
     'USD': 1,
     'KES': 100,
     'USH': 3000,
     'TSH': 2000,
-    'RSA': 5
-}
+    'RSA': 5,
+    'EUR': 0.86
+};
 
-function convert(rates = RATES){
-    const money = {
-        currency: document.getElementById('currency').value,
-        amount: parseFloat(document.getElementById('amount').value)
-    }
-
+function convert(money, rates) {
     const factor = money.amount / rates[money.currency];
 
     return Object.entries(rates).map(
-        ([currency,amount]) => [currency, amount*factor]
+        ([currency, amount]) => [currency, amount * factor]
     );
-
 }
 
-function createRow(currency, amount) {
+function createRow(currency, amount, currency_names = NAMES) {
     let row = document.createElement('tr');
-    row.className = "curr";
+    row.className = "dynamic";
 
     let curr = document.createElement('th');
     curr.scope = "row";
@@ -40,38 +36,91 @@ function createRow(currency, amount) {
     amount_elem.innerHTML = amount;
 
     let name = document.createElement('td');
-    name.innerHTML = NAMES[currency];
+    name.innerHTML = currency_names[currency];
 
     row.innerHTML = curr.outerHTML + amount_elem.outerHTML + name.outerHTML;
 
     return row;
 }
 
-function clearTable(table) {
-    const rows = table.getElementsByClassName('curr');
-    Object.entries(rows).forEach(
-        ([_, row]) => row.parentNode.removeChild(row)
+function createOption(value, text) {
+    const option = document.createElement('option');
+
+    option.className = "dynamic";
+    option.value = value;
+    option.innerHTML = text;
+
+    return option;
+}
+
+function deleteChildren(elem) {
+    const children = elem.getElementsByClassName('dynamic');
+    Object.entries(children).forEach(
+        ([_, child]) => child.parentNode.removeChild(child)
     );
 }
 
-function render(currencies) {
-    const table = document.getElementById('converted');
-    
-    clearTable(table);
-    
-    currencies.forEach(
-        ([currency, amount]) => {
-            const row = createRow(currency, amount);
-            table.appendChild(row);
+function renderSelect(select_element, value_dict) {
+    deleteChildren(select_element);
+
+    Object.entries(value_dict).forEach(
+        ([value, content]) => {
+            const option = createOption(value, content);
+            select_element.appendChild(option);
         }
     );
 }
 
-document.getElementById('convert_from')
-        .addEventListener('submit', 
-            (e) => {
-                const converted = convert();
-                render(converted); 
-                e.preventDefault();
-            }
-        );
+function renderTable(table_element, rows_content) {
+    deleteChildren(table_element);
+
+    rows_content.forEach(
+        ([currency, amount]) => {
+            const row = createRow(currency, amount);
+            table_element.appendChild(row);
+        }
+    );
+}
+
+function renderTableCaption(table_element, label, value) {
+    table_element.getElementsByTagName("caption")[0].innerHTML = `${value} ${label} is equivalent to...`;
+
+}
+
+document.body.onload = function() {
+    const currency_form = document.getElementById('convert_from');
+    const amount_input = document.getElementById('amount');
+    const currency_selector = document.getElementById('currency');
+    const results_table = document.getElementById('converted');
+
+    renderSelect(currency_selector, NAMES);
+
+    amount_input.value = 100;
+    currency_selector.value = "KES";
+
+    let money = {
+        amount: parseFloat(amount_input.value),
+        currency: currency_selector.value
+    };
+
+    let converted = convert(money, RATES);
+
+    renderTable(results_table, converted);
+    renderTableCaption(results_table, money.currency, money.amount);
+
+    currency_form.addEventListener('submit',
+        (e) => {
+            let money = {
+                amount: parseFloat(amount_input.value),
+                currency: currency_selector.value
+            };
+
+            let converted = convert(money, RATES);
+
+            renderTable(results_table, converted);
+            renderTableCaption(results_table, money.currency, money.amount);
+
+            e.preventDefault();
+        }
+    );
+}
