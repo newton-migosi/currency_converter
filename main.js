faf6734b9d0a78af19e123a1145d3b77257e3357
toni_sheen <newtonmigosi@gmail.com>
2018-06-30 16:56:41 +0300

    Matched functions to API

--- main.js
+++ main.js
@@ -1,46 +1,41 @@
-const NAMES = {
-    'USD': "United State Dollars",
-    'KES': "Kenyan Shillings",
-    'USH': "Ugandan Shillings",
-    'RSA': "South African Rand",
-    'TSH': "Tanzanian Shillings",
-    'EUR': "Euro"
-};
+const currecies_url = "https://free.currencyconverterapi.com/api/v5/currencies";
+const rates_url = "https://free.currencyconverterapi.com/api/v3/convert?q=IRR_JMD&compact=ultra";
 
-const RATES = {
-    'USD': 1,
-    'KES': 100,
-    'USH': 3000,
-    'TSH': 2000,
-    'RSA': 5,
-    'EUR': 0.86
-};
-
-function convert(money, rates) {
-    const factor = money.amount / rates[money.currency];
-
-    return Object.entries(rates).map(
-        ([currency, amount]) => [currency, amount * factor]
-    );
+function getRates(currency_from, currency_to) {
+    return {
+        "EUR_BBD": 2.338076,
+        "BTN_BND": 0.019614,
+        "BTN_BND": 0.019614,
+        "XAF_CUP": 0.047248,
+        "USD_FKP": 0.757204,
+        "GIP_HUF": 371.707575,
+        "IRR_JMD": 0.003013,
+    };
 }
 
-function createRow(currency, amount, currency_names = NAMES) {
-    let row = document.createElement('tr');
-    row.className = "dynamic";
+function getCurrencies() {
+    return {
+        "ALL": { "currencyName": "Albanian Lek", "currencySymbol": "Lek", "id": "ALL" }, 
+        "XCD": { "currencyName": "East Caribbean Dollar", "currencySymbol": "$", "id": "XCD" }, 
+        "EUR": { "currencyName": "Euro", "currencySymbol": "€", "id": "EUR" }, 
+        "BBD": { "currencyName": "Barbadian Dollar", "currencySymbol": "$", "id": "BBD" }, 
+        "BTN": { "currencyName": "Bhutanese Ngultrum", "id": "BTN" }, 
+        "BND": { "currencyName": "Brunei Dollar", "currencySymbol": "$", "id": "BND" }, 
+        "XAF": { "currencyName": "Central African CFA Franc", "id": "XAF" }, 
+        "CUP": { "currencyName": "Cuban Peso", "currencySymbol": "$", "id": "CUP" }, 
+        "USD": { "currencyName": "United States Dollar", "currencySymbol": "$", "id": "USD" }, 
+        "FKP": { "currencyName": "Falkland Islands Pound", "currencySymbol": "£", "id": "FKP" }, 
+        "GIP": { "currencyName": "Gibraltar Pound", "currencySymbol": "£", "id": "GIP" }, 
+        "HUF": { "currencyName": "Hungarian Forint", "currencySymbol": "Ft", "id": "HUF" }, 
+        "IRR": { "currencyName": "Iranian Rial", "currencySymbol": "﷼", "id": "IRR" }, 
+        "JMD": { "currencyName": "Jamaican Dollar", "currencySymbol": "J$", "id": "JMD" },
+        "AUD": { "currencyName": "Australian Dollar", "currencySymbol": "$", "id": "AUD" },
+    };
+}
 
-    let curr = document.createElement('th');
-    curr.scope = "row";
-    curr.innerHTML = currency;
-
-    let amount_elem = document.createElement('td');
-    amount_elem.innerHTML = amount;
-
-    let name = document.createElement('td');
-    name.innerHTML = currency_names[currency];
-
-    row.innerHTML = curr.outerHTML + amount_elem.outerHTML + name.outerHTML;
-
-    return row;
+function convert(money, target_currency, rates) {
+    const index = `${money.currency}_${target_currency}`;
+    return { amount: money.amount * rates[index], currency: target_currency };
 }
 
 function createOption(value, text) {
@@ -64,63 +59,53 @@ function renderSelect(select_element, value_dict) {
     deleteChildren(select_element);
 
     Object.entries(value_dict).forEach(
-        ([value, content]) => {
-            const option = createOption(value, content);
+        ([_, details]) => {
+            const option = createOption(details.id, details.currencyName);
             select_element.appendChild(option);
         }
     );
 }
 
-function renderTable(table_element, rows_content) {
-    deleteChildren(table_element);
-
-    rows_content.forEach(
-        ([currency, amount]) => {
-            const row = createRow(currency, amount);
-            table_element.appendChild(row);
-        }
-    );
-}
-
-function renderTableCaption(table_element, label, value) {
-    table_element.getElementsByTagName("caption")[0].innerHTML = `${value} ${label} is equivalent to...`;
-
+function renderResult(display_element, value_dict) {
+    display_element.value = value_dict.amount;
 }
 
 document.body.onload = function() {
-    const currency_form = document.getElementById('convert_from');
-    const amount_input = document.getElementById('amount');
-    const currency_selector = document.getElementById('currency');
-    const results_table = document.getElementById('converted');
+    const conversion_form = document.getElementById('conversion_form');
 
-    renderSelect(currency_selector, NAMES);
+    const amount_from = document.getElementById('amount_from');
+    const currency_from = document.getElementById('currency_from');
 
-    amount_input.value = 100;
-    currency_selector.value = "KES";
+    const amount_to = document.getElementById('amount_to');
+    const currency_to = document.getElementById('currency_to');
 
-    let money = {
-        amount: parseFloat(amount_input.value),
-        currency: currency_selector.value
-    };
+    const currencies = getCurrencies();
 
-    let converted = convert(money, RATES);
+    renderSelect(currency_from, currencies);
+    renderSelect(currency_to, currencies);
 
-    renderTable(results_table, converted);
-    renderTableCaption(results_table, money.currency, money.amount);
-
-    currency_form.addEventListener('submit',
-        (e) => {
-            let money = {
-                amount: parseFloat(amount_input.value),
-                currency: currency_selector.value
+    amount_from.value = 100;
+    currency_from.value = "EUR";
+    
+    conversion_form.addEventListener('submit',
+    (e) => {
+            let money_from = {
+                currency: currency_from.value,
+                amount: amount_from.value
+            };
+            
+            let money_to = {
+                currency: currency_to.value,
+                amount: amount_to.value
             };
 
-            let converted = convert(money, RATES);
+            const conversion_rate = getRates(money_from.currency, money_to.currency);
 
-            renderTable(results_table, converted);
-            renderTableCaption(results_table, money.currency, money.amount);
+            money_to = convert(money_from, money_to.currency, conversion_rate);
+
+            renderResult(amount_to, money_to);
 
             e.preventDefault();
         }
     );
-}
+};
